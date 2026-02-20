5fa9a523d8959b31ff0712d0b7428c197424c428
tsutsumi.s <s.tsutsumi032@gmail.com>
2026-02-20 15:29:43 +0900

    git テンプレ

--- .sample-code/php/includes/head/theme-color.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * テーマカラー出力
- * 
- * このファイルでは、theme-colorメタタグを出力します。
- */
-?>
-<meta name="theme-color" content="#E2EBE6">
-
--- .sample-code/php/src/js/pages/common.js
+++
@@ -1,9 +0,0 @@
-/************************************************************
- * 指定なし画面
- ************************************************************/
-import { BasePageClass } from '../core/BasePage.js';
-
-// BasePageClassをインスタンス化（DOM完了後に自動実行）
-new BasePageClass('body', () => {
-  console.log('common');
-});
