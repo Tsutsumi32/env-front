2b0f905168424d23034f72a8a40403f8787e331f
tsutsumi.s <s.tsutsumi032@gmail.com>
2026-03-07 00:49:43 +0900

    ubuntu nodeに変更

--- php/docker/php/docker-entrypoint.sh
+++ php/docker/php/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 # 環境変数からUID/GIDを取得（デフォルト値は1000）
-APPUSER_UID=${UID:-1000}
-APPUSER_GID=${GID:-1000}
+APPUSER_UID=${HOST_UID:-1000}
+APPUSER_GID=${HOST_GID:-1000}
 
 # /var/wwwの所有権をappuserに設定（volumesでマウントされたファイルも含む）
 if [ -d /var/www ]; then
